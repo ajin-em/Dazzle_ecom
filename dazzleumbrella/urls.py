d9f86e33fa1df8c5b7dfd64e37a48d784684aa35
Ajin EM <ajinem2@gmail.com>
2024-03-19 12:32:30 +0530

    change in static root

--- dazzleumbrella/urls.py
+++ dazzleumbrella/urls.py
@@ -26,7 +26,5 @@ urlpatterns = [
         # path("__debug__/", include("debug_toolbar.urls")),
     ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
