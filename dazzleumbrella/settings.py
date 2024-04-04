c1b961c3e5aa3d019fde32e8ccaf57f6de126439
ajin-em <ajinem2@gmail.com>
2024-04-04 14:51:23 +0530

    set debug false

--- dazzleumbrella/settings.py
+++ dazzleumbrella/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # DEBUG_TOOLBAR_CONFIG = {
 #     'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,
